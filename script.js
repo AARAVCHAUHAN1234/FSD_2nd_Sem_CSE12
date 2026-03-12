8432dcb57601168aa8c78e79b2aeae3ffe84ea38
AARAVCHAUHAN1234 <aarav.25b01010006@abes.ac.in>
2026-03-12 10:54:02 +0530

    js

---
+++ script.js
@@ -0,0 +1,72 @@
+// console.log("hello world.")
+// {
+//     var=20;
+//     a="aarav";
+//     console.log(a);
+// }
+// console.log(a);
+// {
+//     let name="aarav";
+//     name="aarav chauhan";
+// }
+// let name="aarav";
+// console.log(name);
+// {const nsme="aarav";
+// console.log(nsme);}
+// console.log(nsme);
+// what is the diffence between var, let and const in javascript?
+// in JS there are 7 types of datatypes.JS
+// Symbol            unique
+// String
+// null
+// number       2^32
+// Boolean
+// BigInt
+// undefined
+// primitive and non primitive datatypes 
+// primitivee are pre defined datatypes in JS and non primitive are user defined datatypes in JS
+// non primitive are objects, arrays and functions in JS
+// let name="a";
+// console.log(name);
+// let newmane="aarav chauhan";
+// console.log(newmane);
+// let a=10;
+// let b=5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a++);
+// console.log(a--);
+// console.log(a);
+// let a=10;
+// a+=5;
+// console.log(a);
+// a*=2;
+// console.log(a);
+// console.log("hello world.");
+// let a=5;
+// if(a%2===0){
+//     console.log("even number");
+// }
+// else{
+//     console.log("odd");
+// }
+// let sum=0;
+// for(let i=0;i<5;i++){
+//     console.log(i+1);
+//     sum+=i;
+// }
+// console.log("the sum is",sum);
+// s="aarav";
+// for(let a of s){
+//     console.log(a);
+// }
+
+//arrays in js.
+let arr=[];
+console.log(arr);
+let arr1=[1,2,3,4,5,6];
+console.log(arr1);
+
+
