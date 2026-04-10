5e5796711569fd6dd436baaf4c774c9837009c1e
AARAVCHAUHAN1234 <aarav.25b01010006@abes.ac.in>
2026-04-10 09:41:17 +0530

    new

---
+++ javascript/array.js
@@ -0,0 +1,28 @@
+/*let person={
+    name:"aarav",
+    age:30,
+    city:"newyork"
+};
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+
+let arr=[1,2,3,4,5,6,7,6];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[3]);
+
+
+let mixedarray=[1,"arrat",true,{name:"alice"},[1,2,3,4,5]];
+console.log(mixedarray[0]);
+console.log(mixedarray[1]);
+console.log(mixedarray[2]);
+console.log(mixedarray[3]);
+console.log(mixedarray[4]);*/
+
+//map on array
+let num;
+let array=[1,2,3,4,5,6];
+let squaredarray=array.map(num=>num*num);  //why can we use only this =>. npthing lse
+console.log(squaredarray);
+console.log(array);
