18b27722f43321b75d4baeaab5c1e7b8e3e3c97f
AARAVCHAUHAN1234 <aarav.25b01010006@abes.ac.in>
2026-05-07 12:17:28 +0530

    adding css

--- react/Exp-7/src/App.jsx
+++ react/Exp-7/src/App.jsx
@@ -1,17 +1,35 @@
-import React from "react";
-import StudentCard from "./StudentCard.jsx";
-import "./App.css";
+// import React from "react";
+// import StudentCard from "./StudentCard.jsx";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <h1 className="heading">
+//         Student Card
+//       </h1>
+
+//       <StudentCard name="Aarav" marks={30} grade="A" />
+//       <StudentCard name="Arnav" marks={40} grade="A" />
+//       <StudentCard name="Bob" marks={50} grade="B" />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1 className="heading">
-        Student Card
-      </h1>
+  const [isOn, setIsOn] = useState(false);
 
-      <StudentCard name="Aarav" marks={30} grade="A" />
-      <StudentCard name="Arnav" marks={40} grade="A" />
-      <StudentCard name="Bob" marks={50} grade="B" />
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Toggle Button</h1>
+
+      <button onClick={() => setIsOn(!isOn)}>
+        {isOn ? "ON" : "OFF"}
+      </button>
     </div>
   );
 }
