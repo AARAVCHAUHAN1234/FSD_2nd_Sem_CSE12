0fa29ed619a49e86d9593874b70e0f4d69076bc1
AARAVCHAUHAN1234 <aarav.25b01010006@abes.ac.in>
2026-05-07 11:48:59 +0530

    adding css

--- react/Exp-7/src/App.jsx
+++ react/Exp-7/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import StudentCard from "./StudentCard.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Student Card</h1>
+    <div className="container">
+      <h1 className="heading">
+        Student Card
+      </h1>
 
       <StudentCard name="Aarav" marks={30} grade="A" />
       <StudentCard name="Arnav" marks={40} grade="A" />
