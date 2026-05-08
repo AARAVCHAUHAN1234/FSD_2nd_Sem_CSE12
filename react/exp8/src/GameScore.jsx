a30eda81ec5454fe18ff1fa1b0b0105604d5ba18
AARAVCHAUHAN1234 <aarav.25b01010006@abes.ac.in>
2026-05-08 09:38:24 +0530

    experiment 8

--- react/exp8/src/GameScore.jsx
+++ react/exp8/src/GameScore.jsx
@@ -1,10 +1,24 @@
 import {useState} from "react";
 function Gamestart(){
   const[score,setScore]=useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
   return (
     <div>
       <h1>Current Score:{score}</h1>
       <button onClick={()=>setScore(Score+1)}>Score a point</button>
+      <button onClick={increment}>Increment</button>
+
+
     </div>
   );
 }
