46ee7ba4c6555ac8c1de49f3923890c45c01b5f1
AARAVCHAUHAN1234 <aarav.25b01010006@abes.ac.in>
2026-05-08 09:23:02 +0530

    experiment 8

--- react/My_app/src/main.jsx
+++ react/My_app/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import GameScore from './GameScore.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <GameScore/>
+    <App />
+ 
   </StrictMode>,
 )
